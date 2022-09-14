3d985c9e46081e84e400b2589e3ec38566339ddb
Mona Lisa <denise.demirkol@gmail.com>
2022-09-14 18:17:15 +1000

    v4

--- project3/models.py
+++ project3/models.py
@@ -6,8 +6,8 @@ class statepostcodes(db.Model):
 
     state           = db.Column(db.String(250))
     abbreviation    = db.Column(db.String(10))
-    postcode_low    = db.Column(db.integer)  
-    postcode_high   = db.Column(db.integer) 
+    postcode_low    = db.Column(db.Integer)  
+    postcode_high   = db.Column(db.Integer) 
 
     def __repr__(self):
         return '<statepostcodes %r>' % (self.name)
