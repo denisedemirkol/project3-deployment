4b0908e2abd98e2199590c08f76e741f06bc4f23
Mona Lisa <denise.demirkol@gmail.com>
2022-09-14 17:50:06 +1000

    Update app.py

--- project3/app.py
+++ project3/app.py
@@ -28,16 +28,6 @@ db = SQLAlchemy(app)
 
 # flask setup
 
-connection_string = f"postgres:{DBPass}@localhost:5432/Project3"
-engine = create_engine(f'postgresql://{connection_string}')
-conn = engine.connect()
-
-# reflect an existing database into a new model
-base = automap_base()
-# reflect the tables
-base.prepare(engine, reflect=True)
-
-australia_healthsites = base.classes.australia_healthsites
 
 app = Flask(__name__)
 
@@ -55,72 +45,72 @@ def homepage():
     )
 
 
-@app.route("/api/healthcaretypes")
-def healthcaretypes():
-    healthcaretype = pd.read_sql("SELECT DISTINCT meta_healthcare FROM australia_healthsites", conn)
-    print(type(healthcaretype))
-    return healthcaretype.to_json()
+# @app.route("/api/healthcaretypes")
+# def healthcaretypes():
+#     healthcaretype = pd.read_sql("SELECT DISTINCT meta_healthcare FROM australia_healthsites", conn)
+#     print(type(healthcaretype))
+#     return healthcaretype.to_json()
 
 
 
-@app.route("/api/v0/healthsites")
-def healthsites():
+# @app.route("/api/v0/healthsites")
+# def healthsites():
 
-    sqltext1 = "Select ah.lat,ah.lon,ah.osm_id, ah.completeness,ah.loc_amenity, ah.access_hours, ah.addr_postcode, ah.meta_healthcare, ah.loc_name ,"
-    sqltext2 = "spc.state, spc.abbreviation From australia_healthsites ah Left Join StatePostCodes   spc on to_number(addr_postcode,'9999') Between spc.postcode_low and spc.postcode_high "
-    sqltext3 = "Where lat is not null and lon is not null and substring(COALESCE(addr_postcode,'0'),1,1) IN ('0','1','2','3','4','5','6','7','8','9')"
+#     sqltext1 = "Select ah.lat,ah.lon,ah.osm_id, ah.completeness,ah.loc_amenity, ah.access_hours, ah.addr_postcode, ah.meta_healthcare, ah.loc_name ,"
+#     sqltext2 = "spc.state, spc.abbreviation From australia_healthsites ah Left Join StatePostCodes   spc on to_number(addr_postcode,'9999') Between spc.postcode_low and spc.postcode_high "
+#     sqltext3 = "Where lat is not null and lon is not null and substring(COALESCE(addr_postcode,'0'),1,1) IN ('0','1','2','3','4','5','6','7','8','9')"
 
-    sqltext = sqltext1 + sqltext2 +sqltext3
-    print(sqltext)
+#     sqltext = sqltext1 + sqltext2 +sqltext3
+#     print(sqltext)
 
-    df = pd.read_sql(sqltext, conn)  
+#     df = pd.read_sql(sqltext, conn)  
     
-    data = []
+#     data = []
 
-    for index, row in df.iterrows():
+#     for index, row in df.iterrows():
     
 
-        data.append([{
-            "lat": row['lat'],
-            "lon": row['lon'],
-            "osm_id": row['osm_id'],
-            "completeness": row['completeness'],
-            "loc_amenity":row['loc_amenity'],
-            "access_hours":row['access_hours'],
-            "addr_postcode":row['addr_postcode'],
-            "meta_healthcare":row['meta_healthcare'],
-            "loc_name":row['loc_name'],
-            "state_name": row['state'], 
-            "state_code": row['abbreviation'],                      
-        }])
+#         data.append([{
+#             "lat": row['lat'],
+#             "lon": row['lon'],
+#             "osm_id": row['osm_id'],
+#             "completeness": row['completeness'],
+#             "loc_amenity":row['loc_amenity'],
+#             "access_hours":row['access_hours'],
+#             "addr_postcode":row['addr_postcode'],
+#             "meta_healthcare":row['meta_healthcare'],
+#             "loc_name":row['loc_name'],
+#             "state_name": row['state'], 
+#             "state_code": row['abbreviation'],                      
+#         }])
 
 
 
 
 
-    return jsonify(data)
+#     return jsonify(data)
 
-    #return df.to_json()
-
-
-@app.route("/api/v0/metaoperators")
-def metaoperators():
-
-    
-    sqltext = "Select * From meta_operators_v"
-    df = pd.read_sql(sqltext, conn)  
-
-    return Response(df.to_json(orient="records"), mimetype='application/json')
+#     #return df.to_json()
 
 
-@app.route("/api/v0/statestats")
-def statestats():
+# @app.route("/api/v0/metaoperators")
+# def metaoperators():
 
     
-    sqltext = "Select * From statestats_v"
-    df = pd.read_sql(sqltext, conn)  
+#     sqltext = "Select * From meta_operators_v"
+#     df = pd.read_sql(sqltext, conn)  
 
-    return Response(df.to_json(orient="records"), mimetype='application/json')
+#     return Response(df.to_json(orient="records"), mimetype='application/json')
+
+
+# @app.route("/api/v0/statestats")
+# def statestats():
+
+    
+#     sqltext = "Select * From statestats_v"
+#     df = pd.read_sql(sqltext, conn)  
+
+#     return Response(df.to_json(orient="records"), mimetype='application/json')
 
 
 if __name__ == '__main__':
