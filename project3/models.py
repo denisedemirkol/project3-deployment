032f2bd6424070a15ddf352233fd411d370195b4
Mona Lisa <denise.demirkol@gmail.com>
2022-09-14 18:40:34 +1000

    v7

--- project3/models.py
+++ project3/models.py
@@ -5,8 +5,8 @@ class statepostcodes(db.Model):
     __tablename__ = 'statepostcodes'
 
     recordid        = db.Column(db.Integer, primary_key=True)
-    state           = db.Column(db.String(250))
-    abbreviation    = db.Column(db.String(10))
+    state           = db.Column(db.String(64))
+    abbreviation    = db.Column(db.String(64))
     postcode_low    = db.Column(db.Integer)  
     postcode_high   = db.Column(db.Integer) 
 
